cd6fa40fbd9a42f01deb2129d3460244f294a706
vaibhavCodian <vaibhavshukla.try@gmail.com>
2024-01-29 11:01:56 +0000

    Succefully Implemented = version.1 of task1

--- task1/backend.tf
+++ task1/backend.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      version = "~> 4.7.0"
-    }
-  }
-}
-
-
-
-
 # terraform {
 #   backend "gcs" {
 #     bucket = "q-inframod-pe-training-tf-state-bkt"
@@ -18,3 +7,10 @@ terraform {
 # }
 
 
+# terraform {
+#   backend "gcs" {
+#     bucket = "q-terraform-vaibhav"
+#     prefix = "1"
+#     impersonate_service_account = "q-petraining-tf-sa@prj-inframod-pe-training-0124.iam.gserviceaccount.com"
+#   }
+# }
