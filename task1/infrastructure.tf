93068814d32fb5c1698db03b8f22fcdaf71cfebc
vaibhavCodian <vaibhavshukla.try@gmail.com>
2024-01-31 19:35:39 +0000

    Variables and Dynamic Block Implementation

--- task1/infrastructure.tf
+++ task1/infrastructure.tf
@@ -2,7 +2,7 @@
 # A. Creating VPC and Subnet in US-central-1
 resource "google_compute_network" "vpc_network" {
   name                    = var.vpc_network_name
-  auto_create_subnetworks = false
+  auto_create_subnetworks = var.vpc_network_autocreate
 }
 
 resource "google_compute_subnetwork" "subnet_us_central1" {
@@ -18,7 +18,7 @@ resource "google_compute_firewall" "allow_iap_firewall_rule" {
   network = google_compute_network.vpc_network.name
 
   allow {
-    protocol = "tcp"
+    protocol = var.tcp
     ports    = var.allow_iap_ports
   }
 
@@ -30,38 +30,38 @@ resource "google_compute_firewall" "internal_communication" {
   network = google_compute_network.vpc_network.self_link
 
   allow {
-    protocol = "icmp"
+    protocol = var.icmp
   }
 
   allow {
-    protocol = "tcp"
-    ports    = ["0-65535"]
+    protocol = var.tcp
+    ports    = var.allow_all_ports
   }
 
   allow {
-    protocol = "udp"
-    ports    = ["0-65535"]
+    protocol = var.udp
+    ports    = var.allow_all_ports
   }
 
   source_ranges = var.source_ranges_internal_comm
 }
 
 resource "google_compute_firewall" "health_check_firewall_rule" {
-  name    = "q-health-check-firewall-rule-vaibhav"
+  name    = var.health_check_firewall_rule_name
   network = google_compute_network.vpc_network.name
 
   allow {
-    protocol = "tcp"
+    protocol = var.tcp
     ports    = var.health_check_ports
   }
 
-  source_ranges = ["0.0.0.0/0"]
+  source_ranges = var.health_check_source
 }
 
 # C. Creating a Compute Engine VM and
 # D. Install nginx on the VM using start-up scripts
 resource "google_compute_instance" "q-terraform-vm" {
-  name         = "q-terraform-instance-vaibhav"
+  name         = var.compute_instance_name
   machine_type = var.machine_type
   zone         = var.zone
 
@@ -83,16 +83,14 @@ resource "google_compute_instance" "q-terraform-vm" {
   metadata_startup_script = var.metadata_startup_script
 
   service_account {
-    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+    scopes = var.service_account_scopes
   }
 
-  tags = ["http-server"]
 }
 
 # E. Create an unmanaged MIG with the above VM
 resource "google_compute_instance_group" "q_umig" {
-  name        = "q-umig"
-  description = "Terraform Unmanagend Instance Group"
+  name        = var.compute_instance_mig_name
   network     = google_compute_network.vpc_network.self_link
 
   instances = [google_compute_instance.q-terraform-vm.self_link]
@@ -100,19 +98,38 @@ resource "google_compute_instance_group" "q_umig" {
 
 # F. Create an HTTP load balancer and attach the UMIG as backend.
 resource "google_compute_http_health_check" "q_health_check" {
-  name               = "q-health-check"
-  port               = 80
-  request_path       = "/"
-  check_interval_sec = 10
+  name               = var.health_check_name
+  port               = var.health_check_ports
+  request_path       = var.health_check_request_path
+  check_interval_sec = var.health_check_interval_sec
 }
 
 resource "google_compute_backend_service" "q_backend_service" {
-  name        = "q-backend-service"
-  protocol    = "HTTP"
-  port_name   = "http"
-  timeout_sec = 10
+  name        = var.compute_backend_service_name
+  protocol    = var.HTTP
+  port_name   = var.compute_backend_service_port_name
+  timeout_sec = var.compute_backend_service_timeout_sec
   health_checks = [google_compute_http_health_check.q_health_check.self_link]
   backend {
     group = google_compute_instance_group.q_umig.self_link
   }
+}
+
+
+resource "google_compute_url_map" "q_url_map" {
+  name            = var.url_map_name
+  default_route_action {
+    backend_service = google_compute_backend_service.q_backend_service.self_link
+  }
+}
+
+resource "google_compute_target_http_proxy" "q_target_http_proxy" {
+  name      = var.target_http_proxy_name
+  url_map   = google_compute_url_map.q_url_map.self_link
+}
+
+resource "google_compute_global_forwarding_rule" "q_forwarding_rule" {
+  name       = var.forwarding_rule_name
+  target     = google_compute_target_http_proxy.q_target_http_proxy.self_link
+  port_range = var.forwarding_rule_port_range
 }
--- task1/variables.tf
+++ task1/variables.tf
@@ -30,6 +30,10 @@ variable "remote_state_bucket" {
 variable "vpc_network_name" {
   default = "q-terraform-network-vaibhav"
 }
+variable "vpc_network_autocreate" {
+  type = bool
+  default = false
+}
 
 variable "subnet_name" {
   default = "q-terraform-subnetwork-vaibhav"
@@ -39,9 +43,29 @@ variable "ip_cidr_range" {
   default = "10.0.0.0/16"
 }
 
+variable "tcp" {
+  default = "tcp"
+}
+
+variable "udp" {
+  default = "udp"
+}
+
+variable "icmp" {
+  default = "icmp"
+}
+
+variable "HTTP" {
+  default = "HTTP"
+}
+
 
 variable "allow_iap_ports" {
   default = ["22"]
+}
+
+variable "allow_all_ports" {
+  default = ["0-65535"]
 }
 
 variable "source_ranges_iap" {
@@ -52,8 +76,24 @@ variable "source_ranges_internal_comm" {
   default = ["10.0.0.0/16"]
 }
 
+variable "health_check_name" {
+  default = "q-health-check"
+}
+
+variable "health_check_source" {
+  default = ["0.0.0.0/0"]
+}
+
 variable "health_check_ports" {
   default = ["80"]
+}
+
+variable "health_check_request_path" {
+  default = "/"
+}
+
+variable "health_check_interval_sec" {
+  default = 10
 }
 
 variable compute_allow_iap_firewall_name {
@@ -76,4 +116,46 @@ variable "metadata_startup_script" {
   default = "sudo apt-get update; sudo apt-get install -y nginx"
 }
 
+variable "health_check_firewall_rule_name" {
+  default = "q-health-check-firewall-rule-vaibhav"
+}
 
+variable "compute_instance_name" {
+  default = "q-terraform-instance-vaibhav"
+}
+
+variable "service_account_scopes" {
+  default = ["https://www.googleapis.com/auth/cloud-platform"]
+}
+
+variable "compute_instance_mig_name" {
+  default = "q-umig"
+}
+
+variable "compute_backend_service_name" {
+  default = "q-backend-service"
+}
+
+variable "compute_backend_service_port_name" {
+  default = "http"
+}
+
+variable "compute_backend_service_timeout_sec" {
+  default = 10
+}
+
+variable "url_map_name" {
+  default = "q-url-map"
+}
+
+variable "target_http_proxy_name" {
+  default = "q-target-http-proxy"
+}
+
+variable "forwarding_rule_name" {
+  default = "q-forwarding-rule"
+}
+
+variable "forwarding_rule_port_range" {
+  default = "80"
+}
